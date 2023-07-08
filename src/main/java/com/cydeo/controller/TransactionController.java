a83d38fabc75fefc93605285c5b4a2844b39b252
irfan <23pointalpha@gmail.com>
2023-07-08 15:58:21 +0300

    Validation implemented for make-transfer (Make Transfer form)

--- src/main/java/com/cydeo/controller/TransactionController.java
+++ src/main/java/com/cydeo/controller/TransactionController.java
@@ -4,8 +4,10 @@ import com.cydeo.model.Account;
 import com.cydeo.model.Transaction;
 import com.cydeo.service.AccountService;
 import com.cydeo.service.TransactionService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,11 +44,16 @@ public class TransactionController {
     // Write a post method, that takes transaction object from the method above
     // Complete the make transfer and return to the same page
     @PostMapping("/transfer")
-    public String postMakeTransfer(@ModelAttribute("transfer") Transaction transaction){
+    public String postMakeTransfer(@Valid @ModelAttribute("transaction") Transaction transaction, BindingResult bindingResult, Model model){
+
+        if (bindingResult.hasErrors()){
+            // we need all accounts (for the sender and receiver dropdowns)
+            model.addAttribute("accounts", accountService.listAllAccount());
+            return "transaction/make-transfer";
+        }
 
         // I have UUID of accounts but I need to provide Account object
         // I need to find Accounts based on the ID that I have and use as a parameter to makeTransfer method
-
         Account sender = accountService.retrieveById(transaction.getSender());
         Account receiver = accountService.retrieveById(transaction.getReceiver());
         transactionService.makeTransfer(sender,receiver,transaction.getAmount(),new Date(),transaction.getMessage());
--- src/main/java/com/cydeo/model/Transaction.java
+++ src/main/java/com/cydeo/model/Transaction.java
@@ -1,5 +1,9 @@
 package com.cydeo.model;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,9 +14,16 @@ import java.util.UUID;
 @Data
 @Builder
 public class Transaction {
+    @NotNull
     private UUID sender;
+    @NotNull
     private UUID receiver;
+    @NotNull
+    @Positive
     private BigDecimal amount;
+    @NotNull
+    @Size(min = 2, max = 250)
+    @Pattern(regexp = "^[a-zA-Z0-9]*$")
     private String message;
     private Date creationDate;
 }
